08056a43a2922d70e8dcb53db3b4438ed0d07467
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-08-06 23:45:52 +0800

    将csv文件改为txt直接存储

--- utlis/analysis_data.py
+++ utlis/analysis_data.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import pandas as pd
 
 class Analysis_Data(object):
 
@@ -55,8 +54,6 @@ class Analysis_Data(object):
                     if line[i]==' ':
                         continue
 
-
-
                     # if line[i]=='”':
                     #     print('出现%s,文件%s,行内容：%s' % (line[i], file, line))
 
@@ -69,8 +66,6 @@ class Analysis_Data(object):
                     if self._find_repetition(line[i], '；', ';'): continue
                     #self._find_repetition(line[i], '', '(')
 
-
-
                     if line[i] in self.words_dict.keys():
                         self.words_dict[line[i]] += 1
                     else:
@@ -80,25 +75,11 @@ class Analysis_Data(object):
             if comma_num < 4:
                 print("文件%s,问题行:%s"%(file, line))
 
-    def datal2csv(self, save_file):
+    def datal2onehot(self, save_file):
 
-        word_num = len(self.words_dict.keys())
-        word_labels = [i for i in range(word_num)]
-        word_keys = [i for i in self.words_dict.keys()]
-
-        words_label = pd.DataFrame({'label':word_labels, 'word':word_keys})
-
-        words_label.to_csv(save_file, index=False)
-
-
-    def data2label(self):
-        self.label = {}
-        if self.words_dict is not None:
-            num = 0
-            for key in self.words_dict.keys():
-                label[key] = num
-                num = num + 1
-        return self.label
+        f = open(save_file, 'w')
+        f.write(str(self.words_dict))
+        f.close()
 
     def _find_repetition(self, line, chinese, engish):
         if line == chinese or line == engish:
@@ -114,7 +95,7 @@ class Analysis_Data(object):
 if __name__ == "__main__":
     a = Analysis_Data('../../train/')
     a.word2dict()
-    a.datal2csv('../word_label.csv')
+    a.datal2onehot('../word_label.txt')
 
     path = '../onehot.txt'
     read = open(path, 'r')
