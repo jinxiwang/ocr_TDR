0d373cdb5df0e229bbd8d05980b2b93863be9ba6
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-08-07 16:34:16 +0800

    将analysis类提出来，并完善transform data类

---
+++ utlis/Transform_Data.py
@@ -0,0 +1,123 @@
+import os
+import numpy as np
+import seaborn as sb
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Transform_Data(object):
+
+    words_dict = {}
+
+    def __init__(self, words_dir=None):
+
+        self.words_dir = words_dir
+
+    def word2dict(self):
+        """
+        提取文本数据中的汉子，并统计每个汉子出现的次数
+        :return:
+        """
+        files_list = os.listdir(self.words_dir)
+        files_list.sort()
+        for file in files_list:
+            if 'txt' in file:
+                self._find_words(file)
+
+        return self.words_dict
+
+    def _find_words(self, file):
+        """
+        从file文件中读取数据，并将数据存放在words_dict中
+        :param file:
+        :return:
+        """
+        lines = open(self.words_dir + file)
+
+        for line in lines:
+            comma_num = 0
+            line = line.replace('\n', '')
+            for i in range(len(line)):
+                # 开始解析label字符
+                if comma_num >= 4:
+
+                    # 用于检测是否包含大小写字符
+                    if line[i].isupper():
+                        print('出现大写%s,文件%s,行内容：%s'%(line[i], file, line))
+                    if line[i].islower():
+                        print('出现小写%s,文件%s,行内容：%s'%(line[i], file, line))
+
+                    # if line[i] == 'e' and line[i+1]=='m' and line[i+2]=='p' and line[i+3]=='t' \
+                    #         and line[i+4]=='y':
+                    #
+                    #     if 'empty' in self.words_dict.keys():
+                    #         self.words_dict['empty'] += 1
+                    #     else:
+                    #         self.words_dict['empty'] = 1
+                    #     break
+                    #
+                    # if line[i]==' ':
+                    #     continue
+
+                    # if line[i]=='”':
+                    #     print('出现%s,文件%s,行内容：%s' % (line[i], file, line))
+
+                    if self._find_repetition(line[i], '，', ','): continue
+                    if self._find_repetition(line[i], '？', '?'): continue
+                    if self._find_repetition(line[i], '。', '.'): continue
+                    if self._find_repetition(line[i], '！', '!'): continue
+                    if self._find_repetition(line[i], '（', '('): continue
+                    if self._find_repetition(line[i], '）', ')'): continue
+                    if self._find_repetition(line[i], '；', ';'): continue
+
+                    if line[i] in self.words_dict.keys():
+                        self.words_dict[line[i]] += 1
+                    else:
+                        self.words_dict[line[i]] = 1
+                if line[i] == ",":
+                    comma_num += 1
+            if comma_num < 4:
+                print("文件%s,问题行:%s"%(file, line))
+
+    def datal2onehot(self, save_file):
+        one_hot = {}
+        if self.words_dict is not None:
+            num = 0
+            for key in self.words_dict.keys():
+                one_hot[key] = num
+                num = num + 1
+
+        f = open(save_file, 'w')
+        f.write(str(one_hot))
+        f.close()
+
+    def _find_repetition(self, line, chinese, engish):
+        if line == chinese or line == engish:
+            if chinese in self.words_dict.keys():
+                self.words_dict[chinese] += 1
+            else:
+                self.words_dict[chinese] = 1
+            # if engish in self.words_dict.keys():
+            #     self.words_dict[engish] += 1
+            # else:
+            #     self.words_dict[engish] = 1
+
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    a = Transform_Data()
+    # dict = a.word2dict()
+    # writer = open("../word_num_dict.txt", 'w')
+    # writer.write(str(dict))
+    # writer.close()
+
+    read = open("../word_num_dict.txt", 'r')
+    data = read.read()
+    data_dict = eval(data)
+    #a.analysis_data_distribution(data_dict)
+    #a.dict2csv(data_dict)
+
+    #a.analysis_data_distribution(dict)
+
+    #a.datal2onehot('../word_label.txt')
--- utlis/analysis_data.py
+++ utlis/analysis_data.py
@@ -1,111 +1,46 @@
 import os
 import numpy as np
+import seaborn as sb
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 class Analysis_Data(object):
+    def __init__(self, words_dict):
 
-    words_dict = {}
+        self.words_dict = words_dict
+        self.csv_data = self._dict2csv()
 
-    def __init__(self, words_dir):
-
-        self.words_dir = words_dir
-
-    def word2dict(self):
+    def analysis_data_distribution(self):
         """
-        提取文本数据中的汉子，并统计每个汉子出现的次数
+        分析数据分布
         :return:
         """
-        files_list = os.listdir(self.words_dir)
-        files_list.sort()
-        for file in files_list:
-            if 'txt' in file:
-                self._find_words(file)
+        if self.words_dict == None:
+            assert 0, print("字典为空，无法分析")
 
-        return self.words_dict
+        words_list = list(self.words_dict.keys())
+        words_num_list = [self.words_dict[key] for key in words_list]
 
-    def _find_words(self, file):
-        """
-        从file文件中读取数据，并将数据存放在words_dict中
-        :param file:
-        :return:
-        """
-        lines = open(self.words_dir + file)
+        sb.distplot(words_num_list, kde=False)
 
-        for line in lines:
-            comma_num = 0
-            line = line.replace('\n', '')
-            for i in range(len(line)):
-                # 开始解析label字符
-                if comma_num >= 4:
+        plt.show()
 
-                    # 用于检测是否包含大小写字符
-                    if line[i].isupper():
-                        print('出现大写%s,文件%s,行内容：%s'%(line[i], file, line))
-                    if line[i].islower():
-                        print('出现小写%s,文件%s,行内容：%s'%(line[i], file, line))
-
-                    # if line[i] == 'e' and line[i+1]=='m' and line[i+2]=='p' and line[i+3]=='t' \
-                    #         and line[i+4]=='y':
-                    #
-                    #     if 'empty' in self.words_dict.keys():
-                    #         self.words_dict['empty'] += 1
-                    #     else:
-                    #         self.words_dict['empty'] = 1
-                    #     break
-                    #
-                    # if line[i]==' ':
-                    #     continue
-
-                    # if line[i]=='”':
-                    #     print('出现%s,文件%s,行内容：%s' % (line[i], file, line))
-
-                    if self._find_repetition(line[i], '，', ','): continue
-                    if self._find_repetition(line[i], '？', '?'): continue
-                    if self._find_repetition(line[i], '。', '.'): continue
-                    if self._find_repetition(line[i], '！', '!'): continue
-                    if self._find_repetition(line[i], '（', '('): continue
-                    if self._find_repetition(line[i], '）', ')'): continue
-                    if self._find_repetition(line[i], '；', ';'): continue
-
-                    if line[i] in self.words_dict.keys():
-                        self.words_dict[line[i]] += 1
-                    else:
-                        self.words_dict[line[i]] = 1
-                if line[i] == ",":
-                    comma_num += 1
-            if comma_num < 4:
-                print("文件%s,问题行:%s"%(file, line))
-
-    def datal2onehot(self, save_file):
-        one_hot = {}
-        if self.words_dict is not None:
-            num = 0
-            for key in self.words_dict.keys():
-                one_hot[key] = num
-                num = num + 1
-
-        f = open(save_file, 'w')
-        f.write(str(one_hot))
-        f.close()
-
-    def _find_repetition(self, line, chinese, engish):
-        if line == chinese or line == engish:
-            if chinese in self.words_dict.keys():
-                self.words_dict[chinese] += 1
-            else:
-                self.words_dict[chinese] = 1
-            # if engish in self.words_dict.keys():
-            #     self.words_dict[engish] += 1
-            # else:
-            #     self.words_dict[engish] = 1
-
-            return True
-        return False
+    def analysis_words_num(self):
+        return self.csv_data.word.size
 
 
-if __name__ == "__main__":
-    a = Analysis_Data('../../train/')
-    dict = a.word2dict()
+    def _dict2csv(self):
+        words_list = list(self.words_dict.keys())
+        words_num_list = [self.words_dict[key] for key in words_list]
 
+        csv_data = pd.DataFrame({'word':words_list, 'word_num':words_num_list})
+        return csv_data
 
+if __name__ == '__main__':
+    read = open("../word_num_dict.txt", 'r')
+    data = read.read()
+    data_dict = eval(data)
 
-    #a.datal2onehot('../word_label.txt')
+    a = Analysis_Data(data_dict)
+    print(a.analysis_words_num())
