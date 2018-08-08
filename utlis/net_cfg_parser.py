11c1ed7229b033d1f62822a3a8a4ffb207d87e19
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-08-08 15:33:33 +0800

    添加cfg文件解析

---
+++ utlis/net_cfg_parser.py
@@ -0,0 +1,30 @@
+import configparser as cp
+
+def parser_cfg_file(cfg_file):
+    net_params = {}
+    train_params = {}
+
+    config = cp.ConfigParser()
+    config.read(cfg_file)
+
+    for section in config.sections():
+        # 获取配置文件中的net信息
+        if section == 'net':
+            for option in config.options(section):
+                net_params[option] = config.get(section,option)
+
+        # 获取配置文件中的train信息
+        if section == 'train':
+            for option in config.options(section):
+                train_params[option] = config.get(section,option)
+
+    return net_params,train_params
+
+if __name__=='__main__':
+    net_params, train_params = parser_cfg_file('../net.cfg')
+
+    print(net_params)
+
+
+
+
